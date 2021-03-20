f76fd1bbe7be93521fa4943f907035848137ebba
Klint <klintwales@gmail.com>
2021-03-20 17:48:42 -0500

    handleClick added to submit button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,31 @@ import React, {useState} from "react";
 
 function App() {
 
-  const [name, setName] = useState(" ");
+  var [name, setName] = useState(" ");
+  var [newName, newSetName] = useState(" ");
 
   function handleChange(event){
     console.log(event.target.value);
     setName(event.target.value);
   }
 
+  function handleClick(){
+    console.log("clicked");
+     newSetName(name);
+     setName("");
+
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input onChange={handleChange}type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {newName}</h1>
+      <input
+      onChange = {handleChange}
+      type="text"
+      placeholder="What's your name?"
+      value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
